adf5bc9e5786dddfaba62c1bf4709fd0e6dc1416
Mighantos-PC <michalsvag@gmail.com>
2021-04-03 18:09:33 +0200

    List of indexes page

--- admin_app/src/App.js
+++ admin_app/src/App.js
@@ -7,21 +7,28 @@ import axios from 'axios';
 class IndexRow extends React.Component {
     constructor(props) {
         super(props);
+        //console.log(props)
     }
 
     render() {
         return (
             <tr>
-                <th scope={"row"}>{this.props.name}</th>
+                <th scope={"row"}>{this.props.data.name}</th>
                 <td>{this.props.running ? 'Running' : 'Indexed'}</td>
-                <td>Last update</td>
-                <td>Duration</td>
-                <td>Dashboards</td>
-                <td>
+                <td>{this.props.data.lastUpdate}</td>
+                <td>{this.props.data.duration}</td>
+                <td>{this.props.data.dashboards.map(dashboard => dashboard + ", ")}</td>
+                <td className={'text-center'}>
                     <Bootstrap.ButtonGroup>
-                        <Bootstrap.Button>Edit</Bootstrap.Button>
-                        <Bootstrap.Button>Update</Bootstrap.Button>
-                        <Bootstrap.Button variant="danger">Delete</Bootstrap.Button>
+                        {/*TODO: redirect to edit site*/}
+                        <Bootstrap.Button onClick={() => alert("todo")}>Edit</Bootstrap.Button>
+                        {/*TODO: update button text*/}
+                        <Bootstrap.Button
+                            onClick={() => axios.post('http://localhost:8080/config/' + this.props.data.name + '/start')}>Update
+                            now</Bootstrap.Button>
+                        <Bootstrap.Button variant="danger"
+                                          onClick={() => axios.delete('http://localhost:8080/config/' + this.props.data.name)}
+                        >Delete</Bootstrap.Button>
                     </Bootstrap.ButtonGroup>
                 </td>
             </tr>
@@ -33,17 +40,19 @@ class IndexTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            configs: {},
+            configs: Array(1),
             running: Array(1)
         }
-        axios.get('http://localhost:8080/config').then((response) => this.setState({configs: response.data}));
-        axios.get('http://localhost:8080/running').then((response) => this.setState({running: response.data}));
+        axios.get('http://192.168.0.101:8080/config').then((response) => this.setState({configs: response.data}))
+        setInterval(() => axios.get('http://192.168.0.101:8080/config').then((response) => this.setState({configs: response.data})), 10000);
+        axios.get('http://192.168.0.101:8080/running').then((response) => this.setState({running: response.data}));
+        setInterval(() => axios.get('http://192.168.0.101:8080/running').then((response) => this.setState({running: response.data})), 10000);
     }
 
-    renderRow(key) {
+    renderRow(index) {
         return (
-            <IndexRow name={key} data={this.state.configs[key]} running={this.state.running.includes(key)}/>
-        )
+            <IndexRow key={index.name} data={index} running={this.state.running.includes(index.name)}/>
+        );
     }
 
     render() {
@@ -60,7 +69,7 @@ class IndexTable extends React.Component {
                 </tr>
                 </thead>
                 <tbody>
-                {Object.keys(this.state.configs).map(key => this.renderRow(key))}
+                {this.state.configs.map(index => this.renderRow(index))}
                 </tbody>
 
             </Bootstrap.Table>
@@ -71,10 +80,23 @@ class IndexTable extends React.Component {
 class App extends React.Component {
     render() {
         return (
-            <Bootstrap.Container>
+            <Bootstrap.Container className={'pt-5'}>
                 <Bootstrap.Row>
                     <Bootstrap.Col>
+                        <h1>
+                            Indexes
+                        </h1>
+                    </Bootstrap.Col>
+                </Bootstrap.Row>
+                <Bootstrap.Row >
+                    <Bootstrap.Col>
                         <IndexTable/>
+                    </Bootstrap.Col>
+                </Bootstrap.Row>
+                <Bootstrap.Row>
+                    <Bootstrap.Col className={'text-right'}>
+                        {/*TODO: redirect to create new*/}
+                        <Bootstrap.Button variant={"success"} className={'mr-3'} onClick={() => alert("todo")}>Create new</Bootstrap.Button>
                     </Bootstrap.Col>
                 </Bootstrap.Row>
             </Bootstrap.Container>
